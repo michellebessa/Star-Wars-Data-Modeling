dbdc94c13f83744ab4d5ea449c6074fbe5cd4422
michellebessa <michelleandressa12@gmail.com>
2022-10-27 19:26:13 +0000

    project ready

--- src/models.py
+++ src/models.py
@@ -8,26 +8,66 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+class Character(Base):
+    __tablename__ = 'Character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    height = Column(Integer, nullable=True)
+    mass = Column(Integer, nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    skin_color = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=True)
+    birth_year = Column(Integer, nullable=True)
+    gender = Column(String(250), nullable=True)
+
+
+class Planets(Base):
+    __tablename__ = 'Planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    rotation_period = Column(Integer, nullable=True)
+    orbital_period = Column(Integer, nullable=True)
+    diameter = Column(Integer, nullable=True)
+    climate = Column(String(250), nullable=True)
+    gravity = Column(String(250), nullable=True)
+    terrain = Column(String(250), nullable=True)
+    surface_water = Column(Integer, nullable=True)
+    population = Column(Integer, nullable=True)
+
+
+class Vehicles(Base):
+    __tablename__ = 'Vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    model = Column(String(250), nullable=True)
+    manufacterer = Column(String(250), nullable=True)
+    cost_in_credits = Column(Integer, nullable=True)
+    length = Column(Integer, nullable=True)
+    max_atmosphering_speed = Column(Integer, nullable=True)
+    crew = Column(Integer, nullable=True)
+    passengers = Column(Integer, nullable=True)
+    cargo_capacity = Column(Integer, nullable=True)
+
+
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey('User.id'))
+    favorite_name = Column(String(250), nullable=False)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
